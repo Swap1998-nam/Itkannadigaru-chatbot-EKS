e015e34a768fb032123d4c722f5c8869a000882a
Swapnil Balaganur <swapnilb166@gmail.com>
2026-01-23 18:39:03 +0530

    Add script to install and configure AWS CLI
    Install AWS CLI and configure it for use.

---
+++ scripts/aws_cli.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install unzip curl -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+aws configure
